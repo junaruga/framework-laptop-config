453b602e3923e481975d7c1ff5203f626f6e6f8a
Jun Aruga <jun.aruga@gmail.com>
2023-09-27 01:00:56 +0200

    Update sway config and xorg.conf.d conf files.

---
+++ home/.config/sway/script/bemenu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+bemenu-run \
+  --list 7 \
+  --prompt 'bemenu' \
+  --no-overlap \
+  --fn 'Monospace 18' \
+  --margin 4 \
+  --width-factor 0.5 \
+  --border 4 \
+  --tb '#222222' --tf '#ffffff' \
+  --fb '#222222' --ff '#ffffff' \
+  --hb '#222222' --hf '#ffffff' \
+  --sb '#222222' --sf '#ffffff' \
+  --bdr '#99644c'
