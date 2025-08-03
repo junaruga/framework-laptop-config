b1dfd7f7f541ec7032ae1e12f2e9364feed44846
Jun Aruga <jun.aruga@gmail.com>
2025-08-04 01:49:20 +0200

    Updated sway, waybar configs.

--- home/.config/sway/script/auto_rotate_display.sh
+++ home/.config/sway/script/auto_rotate_display.sh
@@ -4,21 +4,20 @@ set -euo pipefail
 
 monitor-sensor | \
     mawk -W interactive \
-    '/Accelerometer orientation changed:/ { print $NF; fflush();}' | \
-    while read -r line
-do
-   case "$line" in
-   normal)
-       swaymsg output eDP-1 transform 0
-       ;;
-   bottom-up)
-       swaymsg output eDP-1 transform 180
-       ;;
-  right-up)
-       swaymsg output eDP-1 transform 90
-       ;;
-  left-up)
-       swaymsg output eDP-1 transform 270
-       ;;
-   esac
-done
+    '/Accelerometer orientation changed:/ {print $NF; fflush();}' | \
+    while read -r line; do
+        case "$line" in
+        normal)
+            swaymsg output eDP-1 transform 0
+            ;;
+        bottom-up)
+            swaymsg output eDP-1 transform 180
+            ;;
+        right-up)
+            swaymsg output eDP-1 transform 90
+            ;;
+        left-up)
+            swaymsg output eDP-1 transform 270
+            ;;
+        esac
+    done
