1fac3e4ab3e65535668f8c4e0bc006f56b7f2e58
Jun Aruga <jun.aruga@gmail.com>
2025-08-04 07:58:48 +0200

    toggle_virtual_keyboard.sh: Updated.

--- home/script/toggle_virtual_keyboard.sh
+++ home/script/toggle_virtual_keyboard.sh
@@ -4,12 +4,17 @@ set -eu
 
 # Set the wvkbd-mobintl file path.
 KBD_PROG="${HOME}/bin/wvkbd-mobintl"
-KBD_PROG_NAME=$(basename "${KBD_PROG}")
+KBD_PROG_NAME="$(basename "${KBD_PROG}")"
 
 PID="$(pgrep "${KBD_PROG_NAME}")" || :
 if [ "${PID}" = "" ]; then
     "${KBD_PROG}" \
-        -H 500 -L 400 --fn "Monospace 40" --hidden &
+        -H 500 \
+        -L 400 \
+        --fn "Monospace 40" \
+        --hidden \
+        --alpha 204 \
+        &
     PID="$(pgrep "${KBD_PROG_NAME}")"
 fi
 
